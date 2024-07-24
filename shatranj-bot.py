982022020948a588d373312c8eb77c590fd03aef
raagh <rk00968@surrey.ac.uk>
2024-07-24 23:25:22 +0530

    shatranj bot V2 (using minimax with alpha beta pruning)

--- shatranj-bot.py
+++ shatranj-bot.py
@@ -1,63 +1,109 @@
 import chess
-import chess.engine
-
-class ShatranjBot:
-     def __init__(self):
-         self.board = chess.Board()
 
 
-     def uci(self):
-         print("id name ShatranjBot")
-         print("id author Raaghav")
-         print("uciok")
+class Shatranjbot:
+    def __init__(self):
+        self.board = chess.Board()
 
-     def isready(self):
-         print("readyok")
+    def evaluate(self):
+        return sum([self.piece_value(piece) for piece in self.board.piece_map().values()])
 
-     def ucinewgame(self):
-         self.board.reset()
-     #to reset board after each play
+    def piece_value(self, piece):
+        values = {
+            chess.PAWN: 1,
+            chess.KNIGHT: 3,
+            chess.BISHOP: 3,
+            chess.ROOK: 5,
+            chess.QUEEN: 9,
+            chess.KING: 0
+        }
+        return values[piece.piece_type] * (1 if piece.color == chess.WHITE else -1)
 
-     def position(self, command):
-         if "startpos" in command:
-             self.board.reset()
-             moves = command.split("moves") [1] if "moves" in command else ""
-         else:
-             fen = command.split("position fen ")[1].split(" moves")[0]
-             moves = command.split(" moves ")[1] if " moves " in command else ""
-             self.board.set_fen(fen)
+    def minimax(self, depth, maximizing):
+        if depth == 0 or self.board.is_game_over():
+            return self.evaluate()
 
-         for move in moves.split():
-             self.board.push(chess.Move.from_uci(move))
+        if maximizing:
+            max_eval = float('-inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                eval = self.minimax(depth - 1, False)
+                self.board.pop()
+                max_eval = max(max_eval, eval)
+            return max_eval
+        else:
+            min_eval = float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                eval = self.minimax(depth - 1, True)
+                self.board.pop()
+                min_eval = min(min_eval, eval)
+            return min_eval
 
-     def go(self):
-         move = self.board.legal_moves.__next__()
-         print(f"bestmove {move}")
+    def find_best_move(self, depth):
+        best_move = None
+        best_value = float('-inf')
+        for move in self.board.legal_moves:
+            self.board.push(move)
+            board_value = self.minimax(depth - 1, False)
+            self.board.pop()
+            if board_value > best_value:
+                best_value = board_value
+                best_move = move
+        return best_move
 
-     def stop(self):
-         pass
+    def uci(self):
+        print("id name ShatranjBot")
+        print("id author Raaghav")
+        print("uciok")
 
-     def quit(self):
-         exit()
+    def isready(self):
+        print("readyok")
 
-     def process_command(self, command):
-         if command == "uci":
-             self.uci()
-         elif command == "isready":
-             self.isready()
-         elif command == "ucinewgame":
-             self.ucinewgame()
-         elif command.startswith("position"):
-             self.position(command)
-         elif command.startswith("go"):
-             self.go()
-         elif command == "stop":
-             self.stop()
-         elif command == "quit":
-             self.quit()
+    def ucinewgame(self):
+        self.board.reset()
+
+    def position(self, command):
+        if "startpos" in command:
+            self.board.reset()
+            moves = command.split("moves")[1] if "moves" in command else ""
+        else:
+            fen = command.split("position fen ")[1].split(" moves ")[0]
+            moves = command.split(" moves ")[1] if " moves " in command else ""
+            self.board.set_fen(fen)
+
+        for move in moves.split():
+            self.board.push(chess.Move.from_uci(move))
+
+    def go(self):
+        move = self.find_best_move(2)
+        print(f"bestmove {move}")
+
+    def stop(self):
+        pass
+
+    def quit(self):
+        exit()
+
+    def process_command(self, command):
+        if command == "uci":
+            self.uci()
+        elif command == "isready":
+            self.isready()
+        elif command == "ucinewgame":
+            self.ucinewgame()
+        elif command.startswith("position"):
+            self.position(command)
+        elif command.startswith("go"):
+            self.go()
+        elif command == "stop":
+            self.stop()
+        elif command == "quit":
+            self.quit()
+
 
 if __name__ == "__main__":
-    engine = ShatranjBot()
+    engine = Shatranjbot()
     while True:
         try:
             command = input()
